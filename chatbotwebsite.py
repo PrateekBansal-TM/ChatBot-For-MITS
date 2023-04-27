da4b0771f857f94c24e3332880d39b5196adbb09
Prateek Bansal <108628456+PrateekBansal-TM@users.noreply.github.com>
2023-04-28 02:02:36 +0530

    update

--- chatbotwebsite.py
+++ chatbotwebsite.py
@@ -1,31 +1,49 @@
-import random
+function sendMessage() {
+    var userMessage = document.getElementById("user-message").value;
+    var responseContainer = document.getElementById("response-container");
+    var time = new Date().toLocaleTimeString();
 
-# Define a dictionary of responses for the bot to choose from
-responses = {
-    "hi": ["Hello!", "Hi there!", "Hey!"],
-    "how are you": ["I'm good, thanks for asking!", "I'm doing well, how about you?"],
-    "what's your name": ["My name is Chatbot, nice to meet you!"],
-    "bye": ["Goodbye!", "Bye! Take care!"],
-    "default": ["Sorry, I don't understand. Can you please rephrase?", "I'm not sure what you mean."],
-    "mits": ["Madhav Institute of Technology and Science, Gwalior."],
+    var userBubble = document.createElement("div");
+    userBubble.innerHTML = userMessage;
+    userBubble.className = "container";
+    userBubble.style.backgroundColor = "#dcf8c6";
+    userBubble.style.float = "right";
+    userBubble.style.textAlign = "right";
+    userBubble.style.marginRight = "10px";
+    userBubble.style.marginLeft = "50%";
+    userBubble.style.transform = "translate(-50%)";
+
+    var timeSpan = document.createElement("span");
+    timeSpan.innerHTML = time;
+    timeSpan.className = "time-left";
+
+    userBubble.appendChild(timeSpan);
+    responseContainer.appendChild(userBubble);
+
+    // Send the user message to the backend or AI model to get the bot's response
+    // You can use AJAX request or WebSocket connection here
+    // Once you get the bot's response, create a new bubble element for the bot's response and append it to the response container
+
+    // Example code for displaying the bot's response
+    var botMessage = "This is a test response from the bot."; // Replace with the actual bot response
+    var botBubble = document.createElement("div");
+    botBubble.innerHTML = botMessage;
+    botBubble.className = "container";
+    botBubble.style.backgroundColor = "#f1f0f0";
+    botBubble.style.float = "left";
+    botBubble.style.marginLeft = "10px";
+    botBubble.style.marginRight = "50%";
+    botBubble.style.transform = "translate(0%)";
+
+    var botTimeSpan = document.createElement("span");
+    botTimeSpan.innerHTML = time;
+    botTimeSpan.className = "time-right";
+
+    botBubble.appendChild(botTimeSpan);
+    responseContainer.appendChild(botBubble);
+
+    // Clear the input field
+    document.getElementById("user-message").value = "";
+
+    return false; // Prevent the form from submitting and refreshing the page
 }
-
-# Define a function to handle user input and generate bot responses
-def respond(user_input):
-    for key in responses:
-        if key in user_input.lower():
-            return random.choice(responses[key])
-    return random.choice(responses["default"])
-
-# Define a function to start the chatbot
-def start_chatbot():
-    print("Hello! I'm a chatbot. How can I assist you today?")
-    while True:
-        user_input = input("> ")
-        if user_input.lower() == "exit":
-            break
-        bot_response = respond(user_input)
-        print(bot_response)
-
-# Call the start_chatbot function to begin the conversation
-start_chatbot()
